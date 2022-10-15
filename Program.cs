f975debcb7966c3a24b86b7c4ee25b999375fb9e
ekaterina <kostcat-84@mail.ru>
2022-10-15 19:42:02 +0300

    число больше квадрата

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿int numberA = Convert.ToInt32(Console.ReadLine());
+
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int square = numberB*numberB;
+
+if (numberA == square)
+{
+    Console.WriteLine("первое число являетс квадратом второго");
+}
+else
+{
+    Console.WriteLine("первое число не являетс квадратом второго");
+}
